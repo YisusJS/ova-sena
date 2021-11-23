801b20c38dc4d7ce107337b090594f0333ffc701
Edwin J. Páez <edwinjpr11@gmail.com>
2021-11-23 11:32:37 -0500

    Modificación de la vista de deportes en conjunto

--- src/views/chapters/SetSports.vue
+++ src/views/chapters/SetSports.vue
@@ -3,24 +3,27 @@
   <main class="container-motorSkills">
     <section class="module">
       <div class="module__title-container" id="1.1">
-        <h2 class="module__title">Futuro contenido aqui</h2>
+        <h2 class="module__title">Historia</h2>
       </div>
       <div class="module__content">
         <p>
-          lorem
-          gdhsjndsalkdnskndsndaslkndalksndaslkndaslkndaslkndaslkndalkndaslkndaslkdndaslkndaslknd
-          lorem lorem lorem lorem lorem lorem
+          El término Motricidad surge del estudio desde el punto de vista
+          científico de la Educación Física. En el siglo XVII fue a través de
+          una propuesta de John Locke, de la que surgió la denominada Educación
+          Física, con el propósito de dominar y adiestrar el cuerpo. Locke
+          expresaba el valor de la Educación Física en su trabajo titulado Some
+          Thoughts Concerning Education. Para Locke, la Educación Física era un
+          medio de preparar a las personas ante posibles emergencias de salud,
+          que involucraban dificultades y fatiga. Además, este filósofo-educador
+          vislumbraba a la Educación Física como forma de lograr el desarrollo
+          de un cuerpo vigoroso y listo para cualquier acción que lo necesite.
+          Al respecto, Manuel Sergio (2007) señala que "la Motricidad es el
+          cuerpo en movimiento intencional, procurando la trascendencia, la
+          superación, a nivel integralmente humano y no del físico tan sólo". Es
+          decir, es la propia manifestación de la vida en sus distintos modos de
+          expresarse e impresionarse en ese diálogo yo-otro-cosmos. (Couto y
+          Trigo 2014).
         </p>
-        <div>
-          <img
-            :src="require('@/assets/modules/motorSkills/submodule1.1.png')"
-            alt=""
-          />
-          <img
-            :src="require('@/assets/modules/motorSkills/submodule1.2.png')"
-            alt=""
-          />
-        </div>
       </div>
     </section>
   </main>
