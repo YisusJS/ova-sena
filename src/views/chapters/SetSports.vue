b748cd3ed78f07b9472f0cdabd855f9ac0659b1f
Edwin J. Páez <edwinjpr11@gmail.com>
2021-11-28 15:12:43 -0500

    Terminación del módulo 4

--- src/views/chapters/SetSports.vue
+++ src/views/chapters/SetSports.vue
@@ -6,26 +6,30 @@
     </section>
     <section class="module">
       <div class="module__title-container" id="6.1">
-        <h2 class="module__title">Futuro contenido 1</h2>
+        <h2 class="module__title">Lorem ipsum dolor</h2>
       </div>
       <div class="module__content">
         <p>
-          El término Motricidad surge del estudio desde el punto de vista
-          científico de la Educación Física. En el siglo XVII fue a través de
-          una propuesta de John Locke, de la que surgió la denominada Educación
-          Física, con el propósito de dominar y adiestrar el cuerpo. Locke
-          expresaba el valor de la Educación Física en su trabajo titulado Some
-          Thoughts Concerning Education. Para Locke, la Educación Física era un
-          medio de preparar a las personas ante posibles emergencias de salud,
-          que involucraban dificultades y fatiga. Además, este filósofo-educador
-          vislumbraba a la Educación Física como forma de lograr el desarrollo
-          de un cuerpo vigoroso y listo para cualquier acción que lo necesite.
-          Al respecto, Manuel Sergio (2007) señala que "la Motricidad es el
-          cuerpo en movimiento intencional, procurando la trascendencia, la
-          superación, a nivel integralmente humano y no del físico tan sólo". Es
-          decir, es la propia manifestación de la vida en sus distintos modos de
-          expresarse e impresionarse en ese diálogo yo-otro-cosmos. (Couto y
-          Trigo 2014).
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minima cupiditate quos fuga doloribus repellendus vel accusantium non! Quisquam voluptate obcaecati velit eaque esse maiores molestias molestiae earum, ut sapiente?
+          Aperiam consequuntur nobis quod unde ratione recusandae non suscipit ad dolore molestiae, voluptas blanditiis quam provident. Quidem vero aliquid repudiandae nobis soluta, officiis et officia saepe a similique reprehenderit! Voluptas?
+          Id asperiores consequatur, omnis tempora distinctio aperiam similique nobis corporis sequi tempore eos nulla. Vitae adipisci alias consequatur sunt! Quasi sapiente facilis accusamus aperiam voluptatum ipsam neque hic qui ipsum?
+          Impedit ut neque aliquid officia qui nemo tempora quidem suscipit exercitationem unde ducimus deserunt nobis, inventore tenetur nam perspiciatis accusamus itaque enim sequi voluptatem non! Impedit, explicabo deleniti? Quod, aspernatur.
+          Corporis quod debitis repellendus delectus expedita! Dicta ipsa aliquam ipsam distinctio, ex quisquam voluptatem hic nisi eum quaerat aliquid, illum eius assumenda, reiciendis cum molestias non molestiae aspernatur nam voluptates!
+          Esse necessitatibus laudantium maxime doloribus alias, deleniti impedit quo ea? A iste dolores repudiandae itaque fugiat saepe eos quam omnis enim ullam exercitationem esse, aperiam sed ipsum voluptate qui veritatis.
+          Dolorum, molestiae alias? Recusandae molestiae quasi nostrum enim magni id quibusdam excepturi reprehenderit, maiores voluptates, assumenda quo quae rerum aperiam nobis quas in inventore quia commodi. Enim laudantium voluptates deserunt!
+          Nulla, omnis veniam eligendi nam magnam excepturi ullam repellat, itaque impedit maiores beatae, suscipit possimus sit eveniet alias cumque aspernatur culpa aut cum eum ipsa consequuntur explicabo? Aliquam, dolorum earum.
+          Inventore praesentium voluptatum sint cupiditate repellat! Culpa id, mollitia sit adipisci molestiae quae illum reprehenderit libero. Rerum ducimus cumque at optio placeat voluptate, quibusdam porro ab, veritatis eius itaque! Est.
+          Earum labore, cupiditate nam dolores quo nisi ad neque omnis eum consectetur. Quibusdam sunt eveniet perspiciatis nulla. Voluptates adipisci porro delectus, aliquam, fuga illo repudiandae aliquid autem, ipsa tenetur quaerat.
+          Laborum, velit maxime! Enim reprehenderit repellendus rerum quos? Error perspiciatis id molestias, reprehenderit voluptate necessitatibus ad fugiat minima? Possimus atque minima, corporis maiores cupiditate ratione perspiciatis molestias harum incidunt totam?
+          Dicta mollitia optio, iure adipisci reprehenderit, voluptatibus sed fuga quae qui nobis inventore. Sapiente, animi earum? Ex ipsum placeat, eveniet suscipit mollitia id quasi eaque laboriosam nemo, est aliquam quibusdam.
+          Maxime magnam vero non? Sed, quasi? Veniam est quas, odio tenetur ea dolor blanditiis aliquam maxime, nobis non delectus alias neque sint reiciendis, ab itaque atque voluptatem! Sit, cumque nam.
+          Omnis cum nam veritatis quis consectetur doloremque rerum sit minus asperiores, nobis alias perferendis doloribus quos quo inventore eveniet ipsam modi sunt, saepe non atque. Deleniti repellendus veritatis fugiat ex!
+          Voluptates eos mollitia sed recusandae, a laboriosam sunt quisquam explicabo inventore repellat tempore nostrum sint! Tempora consequuntur, consequatur vel inventore ipsum quos quo nam voluptatum libero culpa reprehenderit impedit enim.
+          Mollitia dolor quod itaque ipsa asperiores alias, ea facilis eos eum magnam veniam voluptatem deserunt at unde id enim perferendis soluta, quisquam dignissimos dolorum! Quos earum rem maxime iste corrupti.
+          Excepturi quidem minus nesciunt consectetur, suscipit neque molestiae dignissimos aspernatur. Eius fugiat eligendi aliquid omnis quos excepturi animi velit distinctio soluta maiores voluptatem nisi minima totam quo magnam, mollitia cum?
+          Doloremque eveniet eos perferendis! Temporibus dolor suscipit odit alias. Dicta neque dolore facere, esse praesentium architecto minima rem unde modi quasi fugiat mollitia laborum veritatis ipsa nisi, quisquam necessitatibus facilis?
+          Ipsam quae, impedit excepturi quos debitis pariatur aliquid dolor cupiditate illo saepe id, sit consequatur in laudantium blanditiis, aliquam culpa earum? Molestias quia qui impedit repellat. In consequuntur voluptate provident.
+          Commodi possimus quasi exercitationem consequatur illo ipsam, explicabo distinctio odit veniam, eius impedit nemo temporibus omnis necessitatibus numquam neque totam, laboriosam iusto. Quidem sit omnis odit dolore amet. Deserunt, nostrum?
         </p>
       </div>
     </section>
